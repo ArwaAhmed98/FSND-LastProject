20121669c271c900474fac5212a2a9afc0ef88db
Arwa <arwaahmedmahmoud99@gmail.com>
2020-10-04 22:08:16 +0200

    commit

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String, Integer,DateTime
 import json
 
 # database_name = "castingagency"
+# database_path = "postgresql://postgres:123@localhost:5432/castingagency" 
 database_path = os.getenv('DATABASE_URL')
 # postgresql://postgres:123@localhost:5432/castingagency
 db = SQLAlchemy()
@@ -18,8 +19,8 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
-
+    # db.create_all() #
+# migrate = Migrate(app, db) # this
 # def db_drop_and_create_all():
 #     db.drop_all()
 #     db.create_all()
